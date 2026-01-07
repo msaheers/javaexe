c9520f05b24af9a1d86b99cbbd11779d4ad0fe03
msaheers <mishalsaheersha10@gmail.com>
2026-01-07 13:42:12 +0000

    java levelling up

---
+++ HelloWorld.java
@@ -0,0 +1,37 @@
+
+/*import java.util.Scanner;
+
+
+public class HelloWorld {
+    public static void main(String args[]) {
+
+
+        Scanner  s = new Scanner( System.in);
+            System.out.println("Enter a number");
+
+            int a = s.nextInt();
+            int b = s.nextInt();
+
+            int c = a + b;
+
+            System.out.println( "Result: " + c);
+
+    }
+}*/
+
+import java.util.Scanner; 
+
+public class HelloWorld {
+    public static void main(String args[]) {
+
+        Scanner s = new Scanner(System.in);
+            System.out.println("Enter a number");
+
+            int a = s.nextInt();
+            int b = s.nextInt();
+
+            int c = a + b; 
+
+            System.out.println( "Result: " + c );
+    }
+}
---
+++ IfSample.java
@@ -0,0 +1,57 @@
+/*import java.util.Scanner;
+
+public class IfSample {
+
+    public static void main(String a[]) {
+
+       Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        int num = sc.nextInt();
+
+        if (num < 0) {
+            System.out.println("Number is Negative");
+        }   else {
+            System.out.println("Number is Positive");
+        }
+
+    }
+} */
+
+
+/*import java.util.Scanner; 
+
+public class IfSample { 
+    public static void main(String a[]) {
+
+        Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number");
+
+            int num = sc.nextInt(); 
+
+            if ( num < 0) {
+                System.out.println("Number is Negative");
+            } else { 
+                System.out.println("Number is Positive");
+            }
+
+    }
+}*/ 
+
+import java.util.Scanner; 
+
+public class IfSample { 
+    public static void main(String a[]) {
+
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("Enter a number");
+
+        int num = sc.nextInt();
+
+        if ( num < 0) {
+            System.out.println("Number is Negative");
+        } else {
+            System.out.println("Number is Positive ");
+        }
+    }
+}
