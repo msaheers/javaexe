e6da71ac75645823d77ef08b6134dd189a404f37
msaheers <mishalsaheersha10@gmail.com>
2026-03-13 17:08:41 +0000

    Updated Java pacakge exe

---
+++ src/hello/hi/Sample.java
@@ -0,0 +1,10 @@
+package hello.hi;
+
+import hello.hey.Hello;
+public class Sample {
+    
+   public static void main(String[] args) {
+       Hello h = new Hello();
+       System.out.println("Import worked bro");
+   }
+}
